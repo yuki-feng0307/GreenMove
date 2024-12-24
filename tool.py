814e7f90efcd268746ef5200161f9d6f4a274e13
yuki <63237456+yuki-feng0307@users.noreply.github.com>
2024-12-24 10:10:40 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -12,10 +12,6 @@ from datetime import datetime
 
 
 def id_home_dict(df):
-    '''
-    :param df:处理后的公园访问数据
-    :return:用户家庭住址dict
-    '''
     filtered_data_home = df.groupby('id').first().reset_index()
     id_home_dict = filtered_data_home.set_index('id')[['home_x', 'home_y']].to_dict(orient='index')
     # print(len(uid_home_dict))
@@ -24,20 +20,13 @@ def id_home_dict(df):
 
 
 def id_home_flow_dict(df):
-    '''
-    :param df:处理后的公园访问数据
-    :return:用户家庭住址dict
-    '''
     filtered_data_home = df.groupby('id').first().reset_index()
     id_home_flow_dict = filtered_data_home.set_index('id')[['home_x', 'home_y']].to_dict(orient='index')
     print('id_home_dict')
     print(len(id_home_flow_dict))
 
-    # 计算每个用户id的频次即用户flow并添加到字典中
     id_counts = df['id'].value_counts().to_dict()
-    # 找到字典中值的最大值
     max_count = max(id_counts.values())
-    print("最大值:", max_count)
     for user_id, count in id_counts.items():
         id_home_flow_dict[user_id]['flow'] = count
     print('id_home_flow_dict')
@@ -50,7 +39,6 @@ def id_home_flow_dict(df):
 def id_attr_dict(df):
     '''
     :param df:anjuke
-    :return:小区id与小区经纬度、totalHouseHoldNum、price的dict
     '''
     id_attr_dict = df.set_index('id')[['lng', 'lat', 'totalHouseHoldNum', 'price']].to_dict(orient='index')
     # print(len(id_attr_dict))
@@ -58,7 +46,6 @@ def id_attr_dict(df):
     return id_attr_dict
 
 
-# 定义函数来为每个多边形计算 area_label
 def calculate_area_label(area):
     if area <= 30000:
         return 'A'
@@ -73,10 +60,6 @@ def calculate_area_label(area):
 
 
 def urban_or_suburban(adcode):
-    '''
-    :param adcode: 行政区域编码
-    :return: 判断城区or郊区
-    '''
     if adcode == '310101' or '310106' or '310104' or '310105' or '310110' or '310109' or '310107':
         return 'urban'
     else:
@@ -84,40 +67,36 @@ def urban_or_suburban(adcode):
 
 
 def distance(lon1, lat1, lon2, lat2):
-    # 将经纬度转换为弧度
     lat1 = radians(lat1)
     lon1 = radians(lon1)
     lat2 = radians(lat2)
     lon2 = radians(lon2)
 
-    # 使用 Haversine 公式计算距离
+    # Haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = 6371 * c  # 地球半径（单位：公里）
+    distance = 6371 * c  # mean radius of the earth(km)
 
     return distance
 
 
 def haversine(coord1, coord2):
-    # 将经纬度转换成弧度
     lat1, lon1 = map(math.radians, coord1)
     lat2, lon2 = map(math.radians, coord2)
 
-    # 计算两点的距离
     dlat = lat2 - lat1
     dlon = lon2 - lon1
     a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
     c = 2 * math.asin(math.sqrt(a))
-    r = 6371  # 地球平均半径，单位为公里
+    r = 6371  
     return c * r
 
 
 def topark_trip_distance(df,loc):
     trip_distance = []
     if(loc == 'work'):
-        # noncommuter的work2distance设为0
         for index, row in df.iterrows():
             if pd.isna(row['work_x']):
                 trip_distance.append(0)
@@ -148,7 +127,6 @@ def Rg_list(df,loc):
     ''' return Rg list '''
 
     Rg_list = []
-    # 按照 "id" 列分组，并得到一系列子 DataFrame
     grouped_df = [group for _, group in df.groupby('id')]
     # everyone Rg
     for i, group_df in enumerate(grouped_df):
@@ -157,9 +135,7 @@ def Rg_list(df,loc):
     return Rg_list
 
 def percentage(dict):
-    # 计算字典值的总和
     total_value = sum(dict.values())
-    # 计算每个字典值的占比，并存储为新的字典
     percentage_dict = {key: value / total_value for key, value in dict.items()}
 
     return percentage_dict
@@ -173,15 +149,11 @@ def visit_ratio(weekday_count):
     return weekday_count
 
 def Merge_inflow(df):
-    # 将时间列转换为时间增量
+    
     df['activity_start_time'] = pd.to_datetime(df['activity_start_time'], format='%Y-%m-%d %H:%M:%S')
-    # 将时间按照每半小时进行归并
     df['activity_start_time'] = df['activity_start_time'].dt.floor('30min')
-    # 统计每个时间段内的频次
     frequency_counts = df['activity_start_time'].value_counts().sort_index()
-    # 将结果存储在字典中
     frequency_dict = frequency_counts.to_dict()
-    # 将字典的键替换为自定义的索引
     new_index = [i / 2 for i in range(48)]
     frequency_dict = dict(zip(new_index, frequency_dict.values()))
     # percentage_dict = percentage(frequency_dict)
@@ -206,17 +178,13 @@ def Merge_flow(df):
     return flow
 
 def Merge_hours(hour):
-    '''
-    以半小时为单位归并
-    '''
-    return math.floor(hour * 2) / 2  # 向下取整到最近的半小时
+
+    return math.floor(hour * 2) / 2  
 
 
 def Merge_minutes(minute):
-    '''
-    以15min为单位归并，返回hour
-    '''
-    merge = math.floor(minute / 15) * 15  # 向下取整到最近的刻钟
+
+    merge = math.floor(minute / 15) * 15  
 
     return merge/60
 
@@ -244,7 +212,6 @@ def Merge_distance(trip_distance):
         # elif i < 100:
         #     merge_distance.append((i // 10) * 10)
 
-    # 统计每个归并后距离的频次，并存储为字典
     merge_distance_dict = dict(Counter(merge_distance))
     merge_distance_dict = dict(sorted(merge_distance_dict.items()))
     percentage_dict = percentage(merge_distance_dict)
@@ -273,7 +240,6 @@ def gini_coefficient(wealths):
 
 
 def convert_to_point(point_str):
-    # 使用正则表达式从字符串中提取坐标
     coords = re.findall(r"\d+\.\d+", point_str)
     if coords:
         x, y = map(float, coords)
